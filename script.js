64d769d9d0162fc8332eb455e8372965cf30eceb
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-05-31 14:20:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
     //Write your code here
+	const table = document.getElementById('sampleTable');
+
+    // Create a new row
+    const newRow = table.insertRow(0); // Insert at the top (index 0)
+
+    // Create two new cells for the new row
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+
+    // Set the text content for the new cells
+    cell1.textContent = 'New Cell1';
+    cell2.textContent = 'New Cell2';
   
   
 }
